679f1f5df19a4fe7d548b23f3f486c0a6ba953f1
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2023-08-11 23:17:58 +0900

    install solargraph

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -106,6 +106,11 @@ nvim_lsp.sumneko_lua.setup {
   },
 }
 
+nvim_lsp.solargraph.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
 -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 --   vim.lsp.diagnostic.on_publish_diagnostics, {
 --   underline = true,
