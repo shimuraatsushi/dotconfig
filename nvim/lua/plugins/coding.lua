18499957d0f5e1c285be66723515d9a37093b303
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2023-12-31 19:07:57 +0900

    add plugins/coding.lua

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  -- override nvim-cmp and add cmp-emoji
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "hrsh7th/cmp-emoji" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "emoji" })
+    end,
+  },
+}
