db03cc367f9c94fb234e83310427b1eaedcfb95f
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2024-01-02 22:26:56 +0900

    that's all

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+return {
+  { "echasnovski/mini.pairs", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+}
