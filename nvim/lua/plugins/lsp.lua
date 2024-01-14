f04738e56aaa24f0c3399be5be4456fa66ca1bc1
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2024-01-15 00:11:24 +0900

    config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -68,19 +68,6 @@ return {
       }
     end,
   },
-  -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-      },
-    },
-  },
-
   -- add solargraph to lspconfig
   {
     "neovim/nvim-lspconfig",
