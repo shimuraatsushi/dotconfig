c7558147b1f882bb59829ef0d431b6d6d62b518d
shimura.atsushi <shimura.atsushi@a-tm.co.jp>
2024-01-15 09:13:48 +0900

    edit config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -75,7 +75,6 @@ return {
     opts = {
       ---@type lspconfig.options
       servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
         solargraph = {
           cmd = {
             "solargraph",
