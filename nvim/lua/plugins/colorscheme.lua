a6072405c84fad68f3456c9409882301a6bf8147
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2024-02-26 20:20:04 +0900

    change colorschema

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   {
-    "polirritmico/monokai-nightasty.nvim",
+    --  "polirritmico/monokai-nightasty.nvim",
+    "craftzdog/solarized-osaka.nvim",
     lazy = true,
     priority = 1000,
   },
