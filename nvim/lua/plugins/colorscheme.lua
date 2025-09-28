55323354c9151b0380af056b094c3957af4ac47b
shimura.atsushi <atsushi.shimura.j.a.n.n.s.f.t.z.n@gmail.com>
2025-09-28 11:37:50 +0900

    edit colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,13 +9,21 @@
 --   end,
 -- }
 
+-- return {
+--   "loctvl842/monokai-pro.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = true,
+--   opts = {},
+--   config = function(_, opts)
+--     vim.cmd([[ colorscheme monokai-pro]])
+--   end,
+-- }
+
 return {
-  "loctvl842/monokai-pro.nvim",
-  lazy = false,
+  "olimorris/onedarkpro.nvim",
   priority = 1000,
-  config = true,
-  opts = {},
   config = function(_, opts)
-    vim.cmd([[ colorscheme monokai-pro]])
+    vim.cmd([[ colorscheme onedark ]])
   end,
 }
